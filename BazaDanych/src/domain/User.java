29eab159ac4172bb180ed242d467a2c9aa086603
zybala <rome01@wp.pl>
2014-09-30 15:10:52 +0200

    hi

---
+++ BazaDanych/src/domain/User.java
@@ -0,0 +1,49 @@
+package domain;
+
+public class User {
+	public String username;
+	public String password;
+	public String email;
+	public String email2;
+	public String security_question;
+	public String security_question_answer;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail2() {
+		return email2;
+	}
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+	public String getSecurity_question() {
+		return security_question;
+	}
+	public void setSecurity_question(String security_question) {
+		this.security_question = security_question;
+	}
+	public String getSecurity_question_answer() {
+		return security_question_answer;
+	}
+	public void setSecurity_question_answer(String security_question_answer) {
+		this.security_question_answer = security_question_answer;
+	}
+	
+
+}
